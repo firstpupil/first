4c9efdfa529e791f6bae8f615c0310c102bc0c41
First <pupil.remapper@gmail.com>
2016-10-20 15:02:29 +0200

    added test

---
+++ setup.bash
@@ -0,0 +1,16 @@
+append pythonpath in .bashrc
+
+Create a symbolib link named libirisao.devices.so to libirisao.devices.1.0.2.5.so 
+  (found in IrisAO_PythonAPI/Library) in ${ANACONDA_SETUP_DIR}/lib  where ANACONDA_SETUP_DIR 
+  is the location of anaconda, e.g   /home/username/anaconda2/
+  (e.g. ln -s IrisAO_PythonAPI/Library/libirisao.devices.1.0.2.5.so ${ANACONDA_SETUP_DIR}/lib/libirisao.devices.so)
+
+drop the .so library in the right folder "/usr/lib/irisao/"
+
+export PYTHONPATH="/home/slacour/Documents/python/libraries/firstctrl:$PYTHONPATH"
+
+mkdir -p ~/Documents/python/libraries/firstctrl
+cd ~/Documents/python/libraries/firstctrl
+git init
+git remote add codemaster https://firstpupil@github.com/firstpupil/first.git
+git pull codemaster master
